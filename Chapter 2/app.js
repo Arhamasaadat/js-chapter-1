acec8e23bdc3234608703ba30eec4a9cd591a8bb
Arhamasaadat <arhamasaadat222@gmail.com>
2026-01-01 13:09:15 -0700

    commit

---
+++ Chapter 2/app.js
@@ -0,0 +1,30 @@
+var username;
+
+var myName ="Arhama Saadat";
+
+var message ="Helw World";
+alert("Message");
+
+var studentName  ="John Doe";
+var studentAge ="15 year old";
+var studentCourse ="Certified Mobile Application Development";
+
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+var PizzaMessage="PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(PizzaMessage);
+
+var Email="My email address is=arhamasaadat@gmail.com";
+alert(Email);
+
+var book ="I am trying to learn from the book A smarter way to learn javascript";
+alert(book);
+
+document.write("Yah!I can write HTML content through JavaScript")
+
+var string="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(string);
+
+document.write("<br><br>--END--");
